2951a7a0788fb67ce58443d9beee8d7ab07d8622
LucasOrsini <leolucasorsini@hotmail.com>
2021-03-09 23:43:00 +0900

    Calculadora finalizada

--- www/components/acao.js
+++ www/components/acao.js
@@ -9,3 +9,35 @@ $(document).on("click","#somar",function(){
   // quando passo valor dentro do .val() ele preenche o input / quando não, ele só armazena
   $("#resultado").val(resultado);
 });
+$(document).on("click","#subtracao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) - parseFloat(valor2);
+  // quando passo valor dentro do .val() ele preenche o input / quando não, ele só armazena
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#multiplicacao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) * parseFloat(valor2);
+  // quando passo valor dentro do .val() ele preenche o input / quando não, ele só armazena
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#divisao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+  // quando passo valor dentro do .val() ele preenche o input / quando não, ele só armazena
+  $("#resultado").val(resultado);
+});
+$(document).on("click","#exponenciacao",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = Math.pow(parseFloat(valor1), parseFloat(valor2));
+  // quando passo valor dentro do .val() ele preenche o input / quando não, ele só armazena
+  $("#resultado").val(resultado);
+});
